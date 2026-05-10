1ad12bb8d730d5075eb297c22237fdc70ffd40da
Gautam Chalotra <141838713+Gautamchalotra@users.noreply.github.com>
2026-05-10 22:08:34 +0530

    Fixed dependencies for deployment

--- app.py
+++ app.py
@@ -326,6 +326,6 @@ def handle_exception(e):
     if hasattr(e, 'code'):
         return f"Error {e.code}: {str(e)}", e.code
     return f"500 Error: An unexpected error occurred: {str(e)}", 500
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
